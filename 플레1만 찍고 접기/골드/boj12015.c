318ded02107d1c2ecefaea5f85620f12afad6433
Rebecca_is_alive_ <minehammer26@gmail.com>
2025-06-04 16:25:37 +0900

    아이디어 떠오름

--- 플레1만 찍고 접기/골드/boj12015.c
+++ 플레1만 찍고 접기/골드/boj12015.c
@@ -1,17 +1,67 @@
 #include <stdio.h>
+// #include "my_algorithm"
 
-int cnt;
-int tmp;
+// int tmp;
+// int tmp_cnt[1000001];
+int n;
+int dp[1000001];
+int arr[1000001];
+int len;
+
+int f1();
+int f_l();
 
 int main() {
-    int arr[1000000] = {0,};
-    int n;
+    // int arr[1000001] = {0,};
+    // int n;
+    // int cnt = 1;
 
-    scanf("%d", &n);
+    // scanf("%d", &n);
     
-    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    // for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
 
-
+    // for (int i = 0; i < n; i++) {
+    //     tmp = arr[i];
+    //     for (int j = i+1; j < n; j++) {
+    //         if (tmp < arr[j]) {
+    //             tmp = arr[j];
+    //             cnt++;
+    //         }
+    //     }
+    //     tmp_cnt[i] = cnt;
+    //     cnt = 1;
+    // }
+    // printf("%d", tmp_cnt[0]);
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("%d\n", len);
 
     return 0;
 }
+
+int f1() {
+    len = 0;
+    for (int i = 1; i <= n; i++) {
+        if (dp[len] < arr[i]) {
+            dp[++len] = arr[i];
+        } else {
+            int lower = f_l();
+            dp[lower] = arr[i];
+
+        }
+    }
+}
+
+int f_l(int start, int casted) {
+    
+}
+
+// testcases
+// 1 2 3 4
+// 4 3 2 1
+// 1 1 1 1
+// 2 9 5 8
+// 83 68 83
+// 
