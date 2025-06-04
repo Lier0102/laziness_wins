74a7b82f9c6390ec635f8191752544c0a691da8a
Rebecca_is_alive_ <minehammer26@gmail.com>
2025-06-04 12:16:56 +0900

    복귀전

---
+++ 플레1만 찍고 접기/hash_.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct _Node {
+    int data;
+    struct _Node *next;
+} Node;
+
+
+
+int main(void) {
+
+
+    return 0;
+}
---
+++ 플레1만 찍고 접기/골드/boj12015.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int cnt;
+int tmp;
+
+int main() {
+    int arr[1000000] = {0,};
+    int n;
+
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+
+
+    return 0;
+}
