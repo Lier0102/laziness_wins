34a11c0aca1e28b417446ac5f1904546003215f9
Rebecca_is_alive_ <minehammer26@gmail.com>
2025-06-09 18:06:42 +0900

    선택정렬 암기 끝

--- 플레1만 찍고 접기/정렬_구현_외우기_프로젝트/selection_sort.c
+++ 플레1만 찍고 접기/정렬_구현_외우기_프로젝트/selection_sort.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h> // 알고리즘 상으로 메모리 할당을 구현하려면 Ptmalloc2의 bin을 구현할 줄 알아야함.
+#include <unistd.h>
+#include <string.h>
+// #include <Windows.h>
+// #include <conio.h>
+// 나는 못함.
 
 int main() {
     int min, idx, tmp;
-    
+    int n;
+    int *arr;
+
+    scanf("%d", &n);
+
+    arr = (int *)malloc(sizeof(int) * n);
+    arr = memset(arr, 0, n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", arr + i);
+    }
+
+    for (int i = 0; i < n; i++) {
+        min = 9999;
+        for (int j = i; j < n; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                idx = j;
+            }
+
+            tmp = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = tmp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+
     return 0;
 }
