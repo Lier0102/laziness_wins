20ed381135ded348d2d4f39319b6116b4d94eda5
Rebecca_is_alive_ <minehammer26@gmail.com>
2025-06-09 21:47:21 +0900

    asdf

--- 플레1만 찍고 접기/정렬_구현_외우기_프로젝트/fuckingImple.h
+++ 플레1만 찍고 접기/정렬_구현_외우기_프로젝트/fuckingImple.h
@@ -3,11 +3,64 @@
 #include <string.h>
 #include <unistd.h>
 
+// jst some bunch of macros
+
+#define INT 0
+#define CHAR 1
+#define FLOAT 2
+#define DOUBLE 3
+#define ARR_1D "a1"
+#define ARR_2D "a2"
+
+// line---
+
+typedef unsigned long long ull; // why Did i...?
+typedef long long ll;
+
 void print_a(const void *a, long long int opt, unsigned long long int size) {
-    if (opt > 3) {
-        for (int i = 0; i < size; i++) {
-            printf("%d ", *(int *)a + i); // haven't tried(just lil imple)
-        }
-        puts("");
+    switch(opt) {
+        case INT:
+            int *arr = (int *)a;
+
+            for (ull i = 0; i < size; i++) {
+                printf("%d ", arr[i]);
+            }
+
+            puts("");
+            break;
+
+        default:
+            puts("Usage: print_a(target, option, size)");
+            break;
     }
+}
+
+void init(void **a, int opt, unsigned long long int size, int value) {
+    if (a == NULL || !size) return;
+
+    switch(opt) {
+        case INT:
+            int *a_t = (int *)malloc(sizeof(int) * size);
+            if (a_t == NULL) return;
+
+            for (ull i = 0; i < size; i++) {
+                a_t[i] = value;
+            }
+            *a = a_t;
+            break;
+        
+        case CHAR:
+            break;
+
+        case FLOAT:
+            break;
+
+        case DOUBLE:
+            break;
+
+        default:
+            puts("Usage: init(target, option, size, value)\t<make sure not to use signed(that contains '-') integer as size!!>");
+            break;
+    }
+
 }
--- 플레1만 찍고 접기/정렬_구현_외우기_프로젝트/insertion_sort.c
+++ 플레1만 찍고 접기/정렬_구현_외우기_프로젝트/insertion_sort.c
@@ -1,23 +1,28 @@
-#include "fuckingImple.h"
+#include "fuckingImple.h" // You HAVE TO, NUH, MUST KNOW WHAT I MEANT TO SAY BELOW
 
 int main() {
     int tmp;
-    int *arr;
+    int *arr; // <--- the very, the most, the first fucking, YES. THE POINTER.
     int n;
     
-    scanf("%d", &n);
+    scanf("%d", &n); // So you have an input!
 
-    arr = (int *)malloc(sizeof(int) * n);
-    memset(arr, 0, sizeof(int) * n);
+    init((void **)&arr, INT, n, 0);
+    if (arr == NULL) { puts("Fucked up ! ! "); return 31337; } // ah, yes, 31337. the sexy number is here.
 
     for (int i = 0; i < n; i++) {
-        scanf("%d", arr + i);
+        if (scanf("%d", &arr[i]) != 1) {
+            puts("[ + ] Well, We're fucked. We now, have to fucking know what the fuck is actually going on here!"); // A normal error message that came from my mind
+            free(arr);
+            return 31337;
+        }
     }
 
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++) { // just the simple insertion sort. but I spent almost 44 mins just because I wANTED TO BUILD MY OWN HEADER???? IN FUCKING C LANG???
         int j = i;
 
-        while (j >= 0 && arr[j] > arr[j + 1]) {
+        while (j >= 0 && arr[j] > arr[j + 1]) { // think i = 1 is the best, huh?
+            // will be updated AS soon...nuh, I don't think so I DON T THINKG S OS GOTHI HGNKJASFID
             tmp = arr[j];
             arr[j] = arr[j + 1];
             arr[j + 1] = tmp;
@@ -25,9 +30,8 @@ int main() {
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    print_a(arr, INT, n);
+    free(arr); // RAHHHHHHHH
 
-    return 0;
+    return 0; // finally, I, yes. YES. YEEEAAAASSSSS
 }
